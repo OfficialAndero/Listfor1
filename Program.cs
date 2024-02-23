6ef29664650082a575083d0a82dbd702bda81011
Opilane TTHK <opilane@tthk.ee>
2024-02-23 11:43:22 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Listfor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var primeNumber = new List<int>();
+            primeNumber.Add(2);
+            primeNumber.Add(3);
+            primeNumber.Add(5);
+            primeNumber.Add(7);
+
+            Console.WriteLine("no of elements:" + primeNumber.Count);
+        }
+    }
+}
